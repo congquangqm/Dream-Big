679118fde5ba10adc5daf49d7e413ca0c43e498a
congquangqm <136634461+congquangqm@users.noreply.github.com>
2023-07-17 13:07:20 +0700

    Fix memory management issues

---
+++ cat_yellow_banana.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Dream Big\n");
+
+	int limit = 2000;
+	for(int i=1; i<=limit; i++)
+	{
+		printf("The sky's the limit. Aim high and push yourself beyond your boundaries.\n");
+	}
+
+	printf("Dream Big and never give up until you reach the stars!\n");
+
+	return 0;
+}
