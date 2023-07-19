f803c56238ada9af93948e45257f8b64cfd4df8d
congquangqm <136634461+congquangqm@users.noreply.github.com>
2023-07-19 18:36:59 +0700

    Improve database indexing

---
+++ flower_queen_house.js
@@ -0,0 +1,475 @@
+// 1
+console.log('Dream Big!');
+
+// 2
+const dreamBigPromise = new Promise((resolve, reject) => {
+  resolve('Dream Big!');
+});
+
+// 3
+const dreamsArePossible = () => {
+  console.log('Dreams are possible with hard work!');
+};
+
+//4 
+const dreamBigger = (dream) => {
+  console.log(`Dream bigger than ${dream}`);
+};
+
+// 5
+const doSomethingToReachYourDream = (action) => {
+  console.log(`Take ${action} to reach your dream.`);
+};
+
+// 6
+const haveTheCourageToFail = () => {
+  console.log('Have the courage to fail to reach your dream!');
+};
+
+// 7
+const beOpenToOpportunities = () => {
+  console.log('Be open to the opportunities that will help you reach you dream.');
+};
+
+// 8
+const beFearless = () => {
+  console.log('Be fearless in the pursuit of what sets your soul on fire.');
+};
+
+// 9
+const neverGiveUp = () => {
+  console.log('Never give up on your dreams!');
+};
+
+// 10
+const trustTheProcess = () => {
+  console.log('Trust the process and stay true to yourself.');
+};
+
+// 11
+const stayPositive = () => {
+  console.log('Stay positive and keep moving forward.');
+};
+
+// 12
+const askForHelp = () => {
+  console.log('Ask for help if you need it.');
+};
+
+// 13
+const takeRisks = () => {
+  console.log('Take risks and don\'t be afraid to make mistakes.');
+};
+
+// 14
+const developSelfBelief = () => {
+  console.log('Develop self-belief and strengthen your inner voice.');
+};
+
+// 15
+const makeAPlan = () => {
+  console.log('Make a plan and define actionable steps to reach your goals.');
+};
+
+// 16
+const workHard = () => {
+  console.log('Work hard and never give up even when the going gets tough.');
+};
+
+// 17
+const makeTheMostOfEveryOpportunity = () => {
+  console.log('Make the most of every opportunity that comes along.');
+};
+
+// 18
+const takeInspirationFromOthers = () => {
+  console.log('Take inspiration from others and use it to reach your dream.');
+};
+
+// 19
+const practicePatience = () => {
+  console.log('Practice patience and remember that success takes time.');
+};
+
+// 20
+const beConfident = () => {
+  console.log('Be confident in your abilities and believe in yourself.');
+};
+
+// 21
+const remindYourselfWhyYouDreamedBigInTheFirstPlace = () => {
+  console.log('Remind yourself why you dreamed big in the first place.');
+};
+
+// 22
+const challengeYourselfAndPushYourLimits = () => {
+  console.log('Challenge yourself and push the limits of what you can do.');
+};
+
+// 23
+const makeTimeToDream = () => {
+  console.log('Make time to dream and never forget your goals.');
+};
+
+// 24
+const stayFocused = () => {
+  console.log('Stay focused and stay on track to acheiving your dream.');
+};
+
+// 25
+const enjoyTheJourney = () => {
+  console.log('Enjoy the journey and savor every moment.');
+};
+
+// 26
+const believeInThePowerOfYourDreams = () => {
+  console.log('Believe in the power of your dreams and make them come true!');
+};
+
+// 27
+const rememberThatAnythingIsPossible = () => {
+  console.log('Remember that anything is possible if you set your mind to it.');
+};
+
+// 28
+const neverStopLearning = () => {
+  console.log('Never stop learning and growing.');
+};
+
+// 29
+const imagineBigger = () => {
+  console.log('Imangine bigger and go beyond your wildest dreams.');
+};
+
+// 30
+const stayMotivated = () => {
+  console.log('Stay motivated and find ways to keep your inspiration alive.');
+};
+
+// 31
+const keepTryingUntilYouSucceed = () => {
+  console.log('Keep trying until you succeed.');
+};
+
+// 32
+const challengeNegativeThinking = () => {
+  console.log('Challenge negative thinking and replace it with positivity.');
+};
+
+// 33
+const learnFromYourMistakes = () => {
+  console.log('Learn from your mistakes and don\'t let them bring you down.');
+};
+
+// 34
+const takeTimeToReflectOnYourProgress = () => {
+  console.log('Take time to reflect on your progress and be proud of what you\'ve accomplished.');
+};
+
+// 35
+const letGoOfThingsYouCan'tControl = () => {
+  console.log('Let go of things you can\'t control and focus on what you can.');
+};
+
+// 36
+const celebrateSmallWins = () => {
+  console.log('Celebrate small wins and stay motivated.');
+};
+
+// 37
+const createABalancedLife = () => {
+  console.log('Create a balanced life and find time for self-care.');
+};
+
+// 38
+const measureSuccessByYourOwnStandards = () => {
+  console.log('Measure success by your own standards and not by what others expect.');
+};
+
+// 39
+const don'tCompareYourselfToOthers = () => {
+  console.log('Don\'t compare yourself to others and focus on your own path.');
+};
+
+// 40
+const don'tBeAfraidToAskForHelp = () => {
+  console.log('Don\'t be afraid to ask for help when you need it.');
+};
+
+// 41
+const findTheRightMentors = () => {
+  console.log('Find the right mentors and surround yourself with positive people.');
+};
+
+// 42
+const findYourPurpose = () => {
+  console.log('Find your purpose and use it to reach your dreams.');
+};
+
+// 43
+const turnYourDreamsIntoGoals = () => {
+  console.log('Turn your dreams into goals and write them down somewhere you can see them every day.');
+};
+
+// 44
+const createAPositiveSupportSystem = () => {
+  console.log('Create a positive support system and tell your family and friends about your dreams.');
+};
+
+// 45
+const don'tLetAnythingStopYou = () => {
+  console.log('Don\'t let anything stop you from reaching your goals.');
+};
+
+// 46
+const enjoyTheLittleThings = () => {
+  console.log('Enjoy the little things and appreciate how far you\'ve come.');
+};
+
+// 47
+const findPassionInEveryTask = () => {
+  console.log('Find passion in every task and use it to reach your dreams.');
+};
+
+// 48
+const believeThatYouCanDoIt = () => {
+  console.log('Believe that you can do it and don\'t let anyone tell you otherwise.');
+};
+
+// 49
+const takeTimeToReflectOnYourJourney = () => {
+  console.log('Take time to reflect on your journey and find ways to stay motivated.');
+};
+
+// 50
+const beGratefulEveryDay = () => {
+  console.log('Be grateful every day and remind yourself why you are working hard.');
+};
+
+// 51
+const stayPositiveAndFocused = () => {
+  console.log('Stay positive and focused and trust that you will get there.');
+};
+
+// 52
+const takeTimeToRestAndRelax = () => {
+  console.log('Take time to rest and relax so you have the energy to reach your goals.');
+};
+
+// 53
+const stayCommittedToYourVision = () => {
+  console.log('Stay committed to your vision and never give up on it.');
+};
+
+// 54
+const reviewYourGoalsRegularly = () => {
+  console.log('Review your goals regularly and make adjustments as needed.');
+};
+
+// 55
+const setRealisticTimelines = () => {
+  console.log('Set realistic timelines and create a plan of action.');
+};
+
+// 56
+const beReadyForOpportunities = () => {
+  console.log('Be ready for opportunities and embrace new challenges.');
+};
+
+// 57
+const takeTimeForPersonalGrowth = () => {
+  console.log('Take time for personal growth and make it a priority.');
+};
+
+// 58
+const appreciateYourJourney = () => {
+  console.log('Appreciate your journey and don\'t forget to celebrate your successes.');
+};
+
+// 59
+const makeTimeForYourDreams = () => {
+  console.log('Make time for your dreams and don\'t let anything stand in your way.');
+};
+
+// 60
+const stayAccountable = () => {
+  console.log('Stay accountable and don\'t be afraid to ask for help.');
+};
+
+// 61
+const setSmallGoals = () => {
+  console.log('Set small goals and break them down into manageable steps.');
+};
+
+// 62
+const rewardYourselfForAchievements = () => {
+  console.log('Reward yourself for achievements and celebrate your wins.');
+};
+
+// 63
+const rememberYourDreamsAreValid = () => {
+  console.log('Remember your dreams are valid and don\'t let anyone tell you otherwise.');
+};
+
+// 64
+const makeYourDreamsYourReality = () => {
+  console.log('Make your dreams your reality and don\'t be afraid to take risks.');
+};
+
+// 65
+const findSolutionsToChallenges = () => {
+  console.log('Find solutions to challenges and don\'t get discouraged.');
+};
+
+// 66
+const createABrainstormingSchedule = () => {
+  console.log('Create a brainstorming schedule and use it to generate new ideas.');
+};
+
+// 67
+const beOptimisticAboutTheFuture = () => {
+  console.log('Be optimistic about the future and believe that anything is possible.');
+};
+
+// 68
+const stayFocusedOnTheBigPicture = () => {
+  console.log('Stay focused on the big picture and don\'t forget why you started.');
+};
+
+// 69
+const practiceSelfCare = () => {
+  console.log('Practice self-care and find time for the things that make you happy.');
+};
+
+// 70
+const takeBreaksAndRelax = () => {
+  console.log('Take breaks and relax so you can stay motivated and focused.');
+};
+
+// 71
+const findYourInnerStrength = () => {
+  console.log('Find your inner strength and don\'t let anything stand in your way.');
+};
+
+// 72
+const lookForOpportunities = () => {
+  console.log('Look for opportunities and don\'t be afraid to try something new.');
+};
+
+// 73
+const practiceMindfulness = () => {
+  console.log('Practice mindfulness and remember to be present in the moment.');
+};
+
+// 74
+const createAMeditationPractice = () => {
+  console.log('Create a meditation practice and stay focused on your goals.');
+};
+
+// 75
+const focusOnTheProcessNotTheOutcome = () => {
+  console.log('Focus on the process not the outcome and remember that it\'s a marathon not a sprint.');
+};
+
+// 76
+const don'tBeAfraidToFail = () => {
+  console.log('Don\'t be afraid to fail and learn from your mistakes.');
+};
+
+// 77
+const doThingsThatExciteYou = () => {
+  console.log('Do things that excite you and don\'t settle for anything less.');
+};
+
+// 78
+const takeTimeForSelfReflection = () => {
+  console.log('Take time for self-reflection and don\'t forget to enjoy the journey.');
+};
+
+// 79
+const beOpenToChange = () => {
+  console.log('Be open to change and find ways to adapt to new situations.');
+};
+
+// 80
+const surroundYourselfWithPeopleWhoBelieveInYou = () => {
+  console.log('Surround yourself with people who believe in you and will support you.');
+};
+
+// 81
+const findTheRightToolsToStayMotivated = () => {
+  console.log('Find the right tools to stay motivated and keep working towards your dream.');
+};
+
+// 82
+const createSystemsToStayOnTrack = () => {
+  console.log('Create systems to stay on track and keep moving forward.');
+};
+
+// 83
+const don'tBeAfraidOfChange = () => {
+  console.log('Don\'t be afraid of change and use it to your advantage.');
+};
+
+// 84
+const setReasonableGoals = () => {
+  console.log('Set reasonable goals and don\'t forget to enjoy the process.');
+};
+
+// 85
+const findYourPath = () => {
+  console.log('Find your path and don\'t be afraid to take risks.');
+};
+
+// 86
+const createAMindsetOfGrowth = () => {
+  console.log('Create a mindset of growth and stay focused on your dreams.');
+};
+
+// 87
+const stayInspired = () => {
+  console.log('Stay inspired and find ways to keep your motivation high.');
+};
+
+// 88
+const rememberThatYouAreCapable = () => {
+  console.log('Remember that you are capable and don\'t be afraid to ask for help.');
+};
+
+// 89
+const usePositiveAffirmations = () => {
+  console.log('Use positive affirmations and remind yourself that you can do it.');
+};
+
+// 90
+const getOutOfYourComfortZone = () => {
+  console.log('Get out of your comfort zone and find new ways to reach your dreams.');
+};
+
+// 91
+const don'tLimitYourDreams = () => {
+  console.log('Don\'t limit your dreams and keep pushing forward.');
+};
+
+// 92
+const beAwareOfYourLimitations = () => {
+  console.log('Be aware of your limitations and find ways to work around them.');
+};
+
+// 93
+const showGratitudeForTheJourney = () => {
+  console.log('Show gratitude for the journey and the opportunities that come your way.');
+};
+
+// 94
+const trustYourGutInstinct = () => {
+  console.log('Trust your gut instinct and don\'t be afraid to act on it.');
+};
+
+// 95
+const listenToYourInnerVoice = () => {
+  console.log('Listen to your inner voice and use it to guide your decisions.');
+};
+
+// 96
+const findYourValue = () =>
